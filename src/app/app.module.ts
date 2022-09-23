df291e3975b2ea8c6f4645666b296562e140f023
Samson AKUMANI <samsakumani@gmail.com>
2022-09-23 14:20:50 +0000

    Setting The AreaModule in the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicModule } from './public/public.module';
+import { ProtectedModule } from './protected/protected.module';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PublicModule,
+    ProtectedModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
